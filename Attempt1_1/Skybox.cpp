62e5bf6af76638cc236f5724a52aa2817ded8e26
Jesse Schollitt <jschollitt@gmail.com>
2017-06-26 20:56:21 +1200

    Fixed edge faces on first row not being added. Fixed skybox rotating with camera by applying the inverse 4x4 matrix to the skybox and making it very large.

--- Attempt1_1/Skybox.cpp
+++ Attempt1_1/Skybox.cpp
@@ -38,8 +38,8 @@ void Skybox::Setup()
 		GL_RGBA, GL_UNSIGNED_BYTE, right.getPixelsPtr()
 	);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
@@ -52,8 +52,8 @@ void Skybox::Setup()
 		GL_RGBA, GL_UNSIGNED_BYTE, left.getPixelsPtr()
 	);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
@@ -66,8 +66,8 @@ void Skybox::Setup()
 		GL_RGBA, GL_UNSIGNED_BYTE, top.getPixelsPtr()
 	);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
@@ -80,8 +80,8 @@ void Skybox::Setup()
 		GL_RGBA, GL_UNSIGNED_BYTE, bottom.getPixelsPtr()
 	);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
@@ -94,8 +94,8 @@ void Skybox::Setup()
 		GL_RGBA, GL_UNSIGNED_BYTE, back.getPixelsPtr()
 	);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
@@ -108,8 +108,8 @@ void Skybox::Setup()
 		GL_RGBA, GL_UNSIGNED_BYTE, front.getPixelsPtr()
 	);
 
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 
@@ -123,9 +123,9 @@ void Skybox::Setup()
 void Skybox::Render(float * matrix)
 {
 	
+	glMultMatrixf(matrix);
 	
 	glEnable(GL_TEXTURE_2D);
-
 	//glEnable(GL_BLEND);
 	//glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
@@ -136,60 +136,60 @@ void Skybox::Render(float * matrix)
 	glBegin(GL_QUADS);
 	// Right face
 	glNormal3f(-1.0f, 0.0f, 0.0f); // point left
-	glTexCoord2f(1.0f, 0.0f); glVertex3f(120.0f, -120.0f, -120.0f);  // Bottom Right Of The Texture and Quad
-	glTexCoord2f(1.0f, 1.0f); glVertex3f(120.0f, 120.0f, -120.0f);  // Top Right Of The Texture and Quad
-	glTexCoord2f(0.0f, 1.0f); glVertex3f(120.0f, 120.0f, 120.0f);  // Top Left Of The Texture and Quad
-	glTexCoord2f(0.0f, 0.0f); glVertex3f(120.0f, -120.0f, 120.0f);  // Bottom Left Of The Texture and Quad
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(100000.0f, -100000.0f, -100000.0f);  // Bottom Right Of The Texture and Quad
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(100000.0f, 100000.0f, -100000.0f);  // Top Right Of The Texture and Quad
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(100000.0f, 100000.0f, 100000.0f);  // Top Left Of The Texture and Quad
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(100000.0f, -100000.0f, 100000.0f);  // Bottom Left Of The Texture and Quad
 	glEnd();
 
 	glBindTexture(GL_TEXTURE_2D, left_handle); // bind to the texture handle target
 	glBegin(GL_QUADS);
 	// Left Face
 	glNormal3f(1.0f, 0.0f, 0.0f); // point right
-	glTexCoord2f(0.0f, 0.0f); glVertex3f(-120.0f, -120.0f, -120.0f);  // Bottom Left Of The Texture and Quad
-	glTexCoord2f(1.0f, 0.0f); glVertex3f(-120.0f, -120.0f, 120.0f);  // Bottom Right Of The Texture and Quad
-	glTexCoord2f(1.0f, 1.0f); glVertex3f(-120.0f, 120.0f, 120.0f);  // Top Right Of The Texture and Quad
-	glTexCoord2f(0.0f, 1.0f); glVertex3f(-120.0f, 120.0f, -120.0f);  // Top Left Of The Texture and Quad
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(-100000.0f, -100000.0f, -100000.0f);  // Bottom Left Of The Texture and Quad
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(-100000.0f, -100000.0f, 100000.0f);  // Bottom Right Of The Texture and Quad
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(-100000.0f, 100000.0f, 100000.0f);  // Top Right Of The Texture and Quad
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(-100000.0f, 100000.0f, -100000.0f);  // Top Left Of The Texture and Quad
 	glEnd();
 
 	glBindTexture(GL_TEXTURE_2D, top_handle); // bind to the texture handle target
 	glBegin(GL_QUADS);
 	// Top Face
 	glNormal3f(0.0f, -1.0f, 0.0f); // point down
-	glTexCoord2f(0.0f, 1.0f); glVertex3f(-120.0f, 120.0f, -120.0f);  // Top Left Of The Texture and Quad
-	glTexCoord2f(0.0f, 0.0f); glVertex3f(-120.0f, 120.0f, 120.0f);  // Bottom Left Of The Texture and Quad
-	glTexCoord2f(1.0f, 0.0f); glVertex3f(120.0f, 120.0f, 120.0f);  // Bottom Right Of The Texture and Quad
-	glTexCoord2f(1.0f, 1.0f); glVertex3f(120.0f, 120.0f, -120.0f);  // Top Right Of The Texture and Quad
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(-100000.0f, 100000.0f, -100000.0f);  // Top Left Of The Texture and Quad
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(-100000.0f, 100000.0f, 100000.0f);  // Bottom Left Of The Texture and Quad
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(100000.0f, 100000.0f, 100000.0f);  // Bottom Right Of The Texture and Quad
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(100000.0f, 100000.0f, -100000.0f);  // Top Right Of The Texture and Quad
 	glEnd();
 
 	glBindTexture(GL_TEXTURE_2D, bottom_handle); // bind to the texture handle target
 	glBegin(GL_QUADS);
 	// Bottom Face
 	glNormal3f(0.0f, 1.0f, 0.0f); // point up
-	glTexCoord2f(1.0f, 1.0f); glVertex3f(-120.0f, -120.0f, -120.0f);  // Top Right Of The Texture and Quad
-	glTexCoord2f(0.0f, 1.0f); glVertex3f(120.0f, -120.0f, -120.0f);  // Top Left Of The Texture and Quad
-	glTexCoord2f(0.0f, 0.0f); glVertex3f(120.0f, -120.0f, 120.0f);  // Bottom Left Of The Texture and Quad
-	glTexCoord2f(1.0f, 0.0f); glVertex3f(-120.0f, -120.0f, 120.0f);  // Bottom Right Of The Texture and Quad
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(-100000.0f, -100000.0f, -100000.0f);  // Top Right Of The Texture and Quad
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(100000.0f, -100000.0f, -100000.0f);  // Top Left Of The Texture and Quad
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(100000.0f, -100000.0f, 100000.0f);  // Bottom Left Of The Texture and Quad
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(-100000.0f, -100000.0f, 100000.0f);  // Bottom Right Of The Texture and Quad
 	glEnd();
 
 	glBindTexture(GL_TEXTURE_2D, back_handle); // bind to the texture handle target
 	glBegin(GL_QUADS);
 	// Back Face
 	glNormal3f(0.0f, 0.0f, -1.0f); // point out of the screen
-	glTexCoord2f(1.0f, 0.0f); glVertex3f(-120.0f, -120.0f, -120.0f);  // Bottom Right Of The Texture and Quad
-	glTexCoord2f(1.0f, 1.0f); glVertex3f(-120.0f, 120.0f, -120.0f);  // Top Right Of The Texture and Quad
-	glTexCoord2f(0.0f, 1.0f); glVertex3f(120.0f, 120.0f, -120.0f);  // Top Left Of The Texture and Quad
-	glTexCoord2f(0.0f, 0.0f); glVertex3f(120.0f, -120.0f, -120.0f);  // Bottom Left Of The Texture and Quad
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(-100000.0f, -100000.0f, -100000.0f);  // Bottom Right Of The Texture and Quad
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(-100000.0f, 100000.0f, -100000.0f);  // Top Right Of The Texture and Quad
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(100000.0f, 100000.0f, -100000.0f);  // Top Left Of The Texture and Quad
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(100000.0f, -100000.0f, -100000.0f);  // Bottom Left Of The Texture and Quad
 	glEnd();
 
 	glBindTexture(GL_TEXTURE_2D, front_handle); // bind to the texture handle target
 	glBegin(GL_QUADS);
 	// Front Face
 	glNormal3f(0.0f, 0.0f, 1.0f);
-	glTexCoord2f(0.0f, 0.0f); glVertex3f(-120.0f, -120.0f, 120.0f);  // Bottom Left Of The Texture and Quad
-	glTexCoord2f(1.0f, 0.0f); glVertex3f(120.0f, -120.0f, 120.0f);  // Bottom Right Of The Texture and Quad
-	glTexCoord2f(1.0f, 1.0f); glVertex3f(120.0f, 120.0f, 120.0f);  // Top Right Of The Texture and Quad
-	glTexCoord2f(0.0f, 1.0f); glVertex3f(-120.0f, 120.0f, 120.0f);  // Top Left Of The Texture and Quad
+	glTexCoord2f(0.0f, 0.0f); glVertex3f(-100000.0f, -100000.0f, 100000.0f);  // Bottom Left Of The Texture and Quad
+	glTexCoord2f(1.0f, 0.0f); glVertex3f(100000.0f, -100000.0f, 100000.0f);  // Bottom Right Of The Texture and Quad
+	glTexCoord2f(1.0f, 1.0f); glVertex3f(100000.0f, 100000.0f, 100000.0f);  // Top Right Of The Texture and Quad
+	glTexCoord2f(0.0f, 1.0f); glVertex3f(-100000.0f, 100000.0f, 100000.0f);  // Top Left Of The Texture and Quad
 	glEnd();
 
 	glEnable(GL_DEPTH_TEST);
